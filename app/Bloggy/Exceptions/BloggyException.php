364e093fb633888413fcb0dce642656e9625cc1a
Simon Dann <simon@photogabble.co.uk>
2013-10-07 21:48:50 +0100

    Adding my exception

--- app/Bloggy/Exceptions/BloggyException.php
+++ app/Bloggy/Exceptions/BloggyException.php
@@ -1,1 +1,35 @@
 <?php namespace Bloggy\Exceptions;
+
+/**
+ * Class BloggyException
+ *
+ * This class extends the core PHP exception class and adds a validation getter and setter so that validation
+ * error data can be preserved when a validation exception is thrown.
+ *
+ * I don't know if this is the very best method for adding this functionality, but I quite like keeping data validation
+ * within my repositories and therefore when they throw an error it should provide context.
+ *
+ * @package Bloggy\Exceptions
+ */
+class BloggyException extends \Exception {
+
+    private $validationErrors;
+
+    /**
+     * @param $errors
+     * @return void
+     */
+    public function setValidationErrors ( \Illuminate\Support\MessageBag $errors )
+    {
+        $this->validationErrors = $errors;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValidationErrors ()
+    {
+        return $this->validationErrors;
+    }
+
+};
